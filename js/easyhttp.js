5c302538f158d77faaf81631ff44f453f128cc88
Benjamel <benjamin.meldal@gmail.com>
2022-12-30 18:23:46 +0100

    push to desktop

--- js/easyhttp.js
+++ js/easyhttp.js
@@ -1,63 +1,141 @@
-function easyHTTP() {
-    this.http = new XMLHttpRequest();
-}
+//ES5 old way 
+
+// function easyHTTP() {
+//     this.http = new XMLHttpRequest();
+// }
 
 // Make an HTTP GET request
-easyHTTP.prototype.get = function(url, callback) {
-    this.http.open('GET', url, true);
+// easyHTTP.prototype.get = function(url, callback) {
+//     this.http.open('GET', url, true);
 
-    let self = this;
-    this.http.onload = function() {
-        if(self.http.status === 200) {
-            callback(null, self.http.responseText);
-        } else {
-            callback('error: ' + self.http.status)
-        }
-    }
+//     let self = this;
+//     this.http.onload = function() {
+//         if(self.http.status === 200) {
+//             callback(null, self.http.responseText);
+//         } else {
+//             callback('error: ' + self.http.status)
+//         }
+//     }
 
-    this.http.send();
-}
+//     this.http.send();
+// }
 
 // Make an HTTP POST request
-easyHTTP.prototype.post = function(url, data, callback) {
-    this.http.open('OPEN', url, true); 
-    this.http.setRequestHeader('Content-type', 'application/json');
+// easyHTTP.prototype.post = function(url, data, callback) {
+//     this.http.open('OPEN', url, true); 
+//     this.http.setRequestHeader('Content-type', 'application/json');
 
-    let self = this;
-    this.http.onload = function() {
-        callback(null, self.http.responseText);
-    }
+//     let self = this;
+//     this.http.onload = function() {
+//         callback(null, self.http.responseText);
+//     }
 
-    this.http.send(JSON.stringify(data));
-}
+//     this.http.send(JSON.stringify(data));
+// }
 
 
 // Make an HTTP PUT request
-easyHTTP.prototype.put = function(url, data, callback) {
-    this.http.open('PUT', url, true); 
-    this.http.setRequestHeader('Content-type', 'application/json');
+// easyHTTP.prototype.put = function(url, data, callback) {
+//     this.http.open('PUT', url, true); 
+//     this.http.setRequestHeader('Content-type', 'application/json');
 
-    let self = this;
-    this.http.onload = function() {
-        callback(null, self.http.responseText);
-    }
+//     let self = this;
+//     this.http.onload = function() {
+//         callback(null, self.http.responseText);
+//     }
 
-    this.http.send(JSON.stringify(data));
-}
+//     this.http.send(JSON.stringify(data));
+// }
 
 // Make an HTTP DELETE request
-easyHTTP.prototype.delete = function(url, data, callback) {
-    this.http.open('DELETE', url, true); 
+// easyHTTP.prototype.delete = function(url, data, callback) {
+//     this.http.open('DELETE', url, true); 
 
-    let self = this;
-    this.http.onload = function() {
-        if(self.http.status === 200) {
-            callback(null, 'Post Deleted')
-        } else {
-            callback('error: ' + self.http.status);
-        }
-    }
+//     let self = this;
+//     this.http.onload = function() {
+//         if(self.http.status === 200) {
+//             callback(null, 'Post Deleted')
+//         } else {
+//             callback('error: ' + self.http.status);
+//         }
+//     }
 
-    this.http.send();
-}
+//     this.http.send();
+// }
+
+//ES6 new way
+
+/** 
+* EasyHTTP Library
+* Library for making HTTP requests
+*
+* @version 2.0.0
+* @Author Benjamin Meldal
+* @License MIT
+*
+**/
+
+//make an HTTP get request
+// class EasyHTTP {
+//     get(url) {
+//         return new Promise((resolve, reject) => {
+//         fetch(url)
+//         .then(res => res.json())
+//         .then(data => resolve(data))
+//         .catch(err => reject(err));
+//         })
+//     }
+// }
+
+//make an HTTP post request
+// class EasyHTTP {
+//     get(url, data) {
+//         return new Promise((resolve, reject) => {
+//         fetch(url, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-type': 'application/json'
+//             },
+//             body: JSON.stringify(data)
+//         })
+//         .then(res => res.json())
+//         .then(data => resolve(data))
+//         .catch(err => reject(err));
+//         })
+//     }
+
+    //make http put request
+    // put(url, data) {
+    //     return new Promise((resolve, reject) => {
+    //     fetch(url, {
+    //         method: 'PUT',
+    //         headers: {
+    //             'Content-type': 'application/json'
+    //         },
+    //         body: JSON.stringify(data)
+    //     })
+    //     .then(res => res.json())
+    //     .then(data => resolve(data))
+    //     .catch(err => reject(err));
+    //     })
+    // }
+
+    //make an http delete request
+//     delete(url) {
+//         return new Promise((resolve, reject) => {
+//         fetch(url, {
+//             method: 'DELETE',
+//             headers: {
+//                 'Content-type': 'application/json'
+//             },
+//             body: JSON.stringify(data)
+//         })
+//         .then(res => res.json())
+//         .then(() => resolve('Resource deleted'))
+//         .catch(err => reject(err));
+//         })
+//     }
+// }
+
+//ES7 newest way 
 
